0b2343b3326052e1950a68c6fa174cc9f098e402
Javier Cazallas <jcazallasc@gmail.com>
2020-05-29 10:59:38 +0200

    Add urls and views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from products.models import Product, Manufacturer
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/product_list.html'
