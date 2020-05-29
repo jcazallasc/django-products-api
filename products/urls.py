561a7bf7ca47e81ef106879096967b325458848c
Javier Cazallas <jcazallasc@gmail.com>
2020-05-29 11:37:50 +0200

    Really simple Django API to list products or get just one

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from products.views import ProductDetailView, ProductListView
+
+from products.views import product_detail, product_list
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='product-list'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('products/', product_list, name='product-list'),
+    path('products/<int:pk>/', product_detail, name='product-detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,39 @@
-from django.views.generic import DetailView, ListView
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
 
-from products.models import Product, Manufacturer
-
-
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = 'products/product_detail.html'
+from products.models import Manufacturer, Product
 
 
-class ProductListView(ListView):
-    model = Product
-    template_name = 'products/product_list.html'
+def product_list(request):
+    products = Product.objects.all()
+    data = {'products': list(products.values())}
+
+    return JsonResponse(data)
+
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        data = {
+            'product': {
+                'name': product.name,
+                'manufacturer': product.manufacturer.name,
+                'description': product.description,
+                'photo': product.photo.url,
+                'price': product.price,
+                'shipping_cost': product.shipping_cost,
+                'quantity': product.quantity,
+            }
+        }
+        response = JsonResponse(data)
+    except ObjectDoesNotExist:
+        return JsonResponse(
+            {
+                'error': {
+                    'code': 404,
+                    'message': 'Product not found',
+                }
+            },
+            status=404,
+        )
+
+    return response
